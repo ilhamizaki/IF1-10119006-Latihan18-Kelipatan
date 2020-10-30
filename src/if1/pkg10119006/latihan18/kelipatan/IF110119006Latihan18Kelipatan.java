1ba534ac631cc355cf2988c71c7dffca6e918c75
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-10-30 14:09:32 +0700

    Add files via upload

---
+++ src/if1/pkg10119006/latihan18/kelipatan/IF110119006Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119006.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Ilham zaki
+ * Kelas    : IF 1
+ * NIM      : 10119006
+ * Deskripsi Program : Tampil Kelipatan 3,5
+ */
+public class IF110119006Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double j = 3.5;
+        for (int i = 1; i*j <= 35.0; i++) {
+            System.out.println(i*j);
+        }
+    }
+    
+}
